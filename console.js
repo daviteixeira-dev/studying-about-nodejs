2cfc4b2b8fedf96bf8e6806e920fc8a823f93b89
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-11 13:27:37 -0300

    Running the first file and understanding globals

---
+++ console.js
@@ -0,0 +1,8 @@
+console.log('Alguma coisa aqui');
+
+// Para execultar, basta realizar o comando 'node (nome do arquivo)'
+// O console faz parte das variaveis globais do node
+console.log(global);
+
+// Podemos conferir todas as variaveis globais do Node em:
+// https://nodejs.org/docs/latest-v17.x/api/globals.html

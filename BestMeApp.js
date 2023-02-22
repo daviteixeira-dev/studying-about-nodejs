cd344051d06dfd96496e7fd643febcfa18f66e5a
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-22 16:48:46 -0300

    Logic for presenting questions and saving answers

--- BestMeApp.js
+++ BestMeApp.js
@@ -8,12 +8,19 @@ const questions = [
 ];
 
 const ask = ( index = 0) => {
-    process.stdout.write(questions[index] + "\n");
+    process.stdout.write("\n\n" + questions[index] + " > ");
 };
 
 ask();
 
+const answers = [];
+
 process.stdin.on("data", data => {
-    process.stdout.write(data.toString().trim() + '\n');
-    process.exit()
-}) // ouvindo os eventos, toda vez que for inseridos dados ele roda a função
+    answers.push(data.toString().trim())
+    if(answers.length < questions.length){
+        ask(answers.length);
+    } else {
+        console.log(answers);
+        process.exit();
+    }
+}); // ouvindo os eventos, toda vez que for inseridos dados ele roda a função

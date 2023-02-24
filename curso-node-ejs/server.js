ab33c137f28b16851b8002b08f41bf7f93852df3
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-23 23:22:22 -0300

    Foreach

--- curso-node-ejs/server.js
+++ curso-node-ejs/server.js
@@ -5,7 +5,39 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("pages/index");
+    const items = [
+        { 
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        { 
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        { 
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        { 
+            title: "A",
+            message: "migável"
+        },
+        { 
+            title: "I",
+            message: "nstall ejs"
+        },
+        { 
+            title: "S",
+            message: "intaxe simples"
+        },
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML utilizando JavaScript";
+
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle
+    });
 })
 
 app.get("/sobre", function(req, res){
@@ -14,4 +46,4 @@ app.get("/sobre", function(req, res){
 
 app.listen(8080);
 
-console.log("Rodando")
+console.log("Rodando");

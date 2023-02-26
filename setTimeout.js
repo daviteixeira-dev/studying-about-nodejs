80af1984b4e53d14f3165a6bd98bf52942fff5a8
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-26 18:32:37 -0300

    setTimeout

--- setTimeout.js
+++ setTimeout.js
@@ -7,3 +7,17 @@ const finished = () => console.log('done!');
 setTimeout(finished, timeOut); // função callback, essa aqui é chamada depois de 3 segundos.
 
 console.log('Mostrar!'); // é mostrada antes da função setTimeout por meio do assíncronismo.
+
+// setTimeout(function, delay)
+
+setTimeout(function(){
+    console.log('depois de 1s')
+}, 1000);
+
+/*
+    A função de argumento do setTimeout é uma 
+    callback, ela vai executar depois de um 
+    certo tempo. No caso do exemplo acima, 
+    a função vai ser executada depois de 1000ms 
+    que é o mesmo que 1s.
+*/

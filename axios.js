7f5cf5f66b3ae4a230f2621ae5da239e2e04bd47
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-27 01:22:14 -0300

    promises with axios

--- axios.js
+++ axios.js
@@ -1,6 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.get('https://api.github.com/users/daviteixeira-btm')
-    .then(res => {
-        console.log(res.data)
-    })
+// O axios devolve uma promessa pendente.
+
+// bagunçado ...
+// axios
+//     .get("https://api.github.com/users/daviteixeira-btm")
+//     .then(response => { 
+//         const user = response.data;
+
+//         axios.get(user.repos_url)
+//             .then(repos => console.log(repos.data))
+//             .catch(error => console.log(error))
+//     })
+//     .catch(error => console.log(error))
+
+axios.get("https://api.github.com/users/daviteixeira-btm")
+    .then(response => axios.get(response.data.repos_url))
+    .then(repos => console.log(repos.data))
+    .catch(error => console.log(error))
+
+// Encadeamento de promessas com o axios

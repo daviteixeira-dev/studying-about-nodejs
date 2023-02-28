2935170efbcacba43565cfa2e497742b4827b2a5
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-28 00:52:11 -0300

    POST

--- APIs/index.js
+++ APIs/index.js
@@ -6,3 +6,9 @@ app.listen('3000')
 
 app.route('/').get((request, response) => response.send("Olá!"))
 app.route('/sobre').get((request, response) => response.send("Este é um sobre da página!"))
+
+// middleware - ponte entre as requisições
+app.use(express.json()) // transforma tudo em json
+
+// app.route('/').post((req, res) => console.log(req.body))
+app.route('/').post((req, res) => res.send(req.body))

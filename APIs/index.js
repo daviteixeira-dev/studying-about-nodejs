cbbf81472b1c4f8730e822e6a08512e19bb6c089
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-02-28 00:39:58 -0300

    GET

--- APIs/index.js
+++ APIs/index.js
@@ -4,3 +4,5 @@ const app = express()
 
 app.listen('3000')
 
+app.route('/').get((request, response) => response.send("Olá!"))
+app.route('/sobre').get((request, response) => response.send("Este é um sobre da página!"))

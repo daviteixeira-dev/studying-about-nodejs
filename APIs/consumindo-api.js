f352eeaafe22d4d67c5861b992fb101b20dd9d19
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-02 02:02:32 -0300

    Consuming API with NodeJS using express and axios

---
+++ APIs/consumindo-api.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+
+app.listen('3000')
+
+app.route('/').get((req, res) => {
+
+    axios.get('https://api.github.com/users/daviteixeira-btm')
+    .then(result => res.send(`<img src="${result.data.avatar_url}" />`))
+    .catch(error => console.log(error))
+
+})
